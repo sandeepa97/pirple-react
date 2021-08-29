a88ea0544be992d463c0016ecb262a7de903ee94
sandeepa97 <lbsandeepa@gmail.com>
2021-08-29 23:46:38 +0530

    phase 2

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
 import React from 'react';
 
 function App() {
-  var style1 = {
-    color: "blue",
-    fontSize: 30,
-    backgroundColor: "orange"
-  }
-
-  var var1 = "Hello World";
-  var var2 = 5 + 7;
-  var var3 = 29;
-
-  var func1 = function func1() {
-    console.log("Some kind of Function!");
-  }
-  var func2 = function func2() {
-    console.log("Some new kind of function");
-  }
-
   return (
     <div>
-      <div>This is JSX</div>
-      <h1 style = {style1} title = "This is the JSX Method">Heading JSX Method</h1>
-      <p id = "para1">This is a Paragraph</p>
-      <p className = "para2">This is 2nd Paragraph</p>
-
-      <p>{var1 + var2}</p>
-      <p>{6 * 8}</p>
-      <p>{var3 !== 19 ? "True" : "False"}</p>
-      <div>
-        <label htmlFor="input1">Some Data</label>
-        <input id="input1" name="input1"></input>
-      </div>
-
-      <button onClick={func1}>Click me!</button>
-      <button onMouseEnter={func2}>Click me!</button>
-    </div>
+    <p>Hello World</p>
+    <p>Hello World from other side</p>
+  </div>
   )
-
 }
 
 export default App;
---
+++ src/Clscom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Clscom extends React.Component {
+    render () {
+        return (
+            <div>
+                <h2>Hello from class Component</h2>
+                <h2>Hello From Other Side!</h2>
+            </div>
+        )
+    }
+}
